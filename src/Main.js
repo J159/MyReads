a98bc5eeaf44621678d4c6e2998b9981b5e644e4
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-05 23:57:55 -0500

    Transfer bookTransfer method to Shelf component

--- src/Main.js
+++ src/Main.js
@@ -11,9 +11,12 @@ class Main extends Component  {
         </div>
         <div className="list-books-content">
           <div>
-            <Shelf name="Currently Reading" books={this.props.books.filter(book => book.shelf === "currentlyReading")} />
-            <Shelf name="Want to Read" books={this.props.books.filter(book => book.shelf === "wantToRead")} />
-            <Shelf name="Read" books={this.props.books.filter(book => book.shelf === "read")} />
+            <Shelf name="Currently Reading" books={this.props.books.filter(book => book.shelf === "currentlyReading")}
+              bookTransfer={this.props.bookTransfer}/>
+            <Shelf name="Want to Read" books={this.props.books.filter(book => book.shelf === "wantToRead")}
+              bookTransfer={this.props.bookTransfer}/>
+            <Shelf name="Read" books={this.props.books.filter(book => book.shelf === "read")}
+              bookTransfer={this.props.bookTransfer}/>
           </div>
         </div>
         {/* Search Button */}
