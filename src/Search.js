43a16ab5fbaa34112843acb2f9653b8fdb5c34f1
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-07 02:04:23 -0500

    Create updateQuery, set query state to empty string

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Search extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query })
+  }
+
   render() {
     return (
       <div className="search-books">
