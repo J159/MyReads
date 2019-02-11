665ccc51002f148662ea7afaf024b0125362f92f
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-11 14:16:53 -0500

    fix: Switch mixed-operators into ternary statement

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || "No Thumbnail"}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : "No Thumbnail"}")` }}></div>
           <div className="book-shelf-changer">
             <select onChange={(event) => this.props.bookTransfer(this.props.book, event.target.value)} value={this.props.book.shelf || 'none'}>
               <option value="move" disabled>Move to...</option>
