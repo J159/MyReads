34978056779062e7f2fde9b95d3bd8dc05f68191
Andy Gaspar <agaspar.artist@gmail.com>
2019-02-11 02:19:26 -0500

    style: Adjust/Remove comments

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
+  // Wrap BrowerRouter around App to utilize react-router-dom
   <BrowserRouter>
     <App />
   </BrowserRouter>,
